02372b46922c26d25d2aca373be0a1578dc9c7d2
Tina Fallahi <fallahi.tina@gmail.com>
2013-10-11 14:02:39 +0100

    Added reading the profile and storing PATH and HOME.

--- bash.c
+++ bash.c
@@ -1,7 +1,47 @@
 #include "bash.h"
 
-main()
+int main()
 {
-    printf("Hello World\n");
+	startup();
+	printf("PATH: %sHOME: %s\n", PATH, HOME);
+    return 0;
+}
 
+// Reading the profile file and storing PATH and HOME variable.
+void startup(){
+	// TODO: Change the file extension.  
+	char fileName[] = "profile.txt";
+    char line [128];
+    FILE *file;
+
+    file = fopen(fileName,"r");
+ 
+    if( file == NULL ) {
+    	perror("Error while opening the file:\n");
+    	exit(EXIT_FAILURE);
+    }
+ 
+    while( fgets (line, sizeof line, file) != NULL) {
+    	if(!strncmp(line, "PATH=", 5)){
+    		memmove(line, line+5, strlen(line));
+    		strcpy(PATH, line);
+    	}
+    	if(!strncmp(line, "HOME=", 5)){
+    		memmove(line, line+5, strlen(line));
+    		strcpy(HOME, line);
+    	}
+    }
+
+    // Checking if PATH or HOME are not defined or incorrectly defined.
+    if(!strcmp(PATH, "")){
+    	perror("PATH not defined correctly in profile:\n");
+    	exit(EXIT_FAILURE);
+    }
+
+    if(!strcmp(HOME, "")){
+    	perror("HOME not defined correctly in profile:\n");
+    	exit(EXIT_FAILURE);
+    }
+ 
+    fclose(file);
 }
--- bash.h
+++ bash.h
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char PATH[128];
+char HOME[128];
+
+void startup();
